7d391155829ecda62091aa49793bcae2216743ea
lovroselic <lovro.selic@gmail.com>
2023-04-26 08:07:51 +0200

    0.14.00 final sandbox

--- CrawlMaster2.js
+++ CrawlMaster2.js
@@ -45,7 +45,7 @@ const INI = {
     FINAL_LEVEL: 1,
 };
 const PRG = {
-    VERSION: "0.13.06",
+    VERSION: "0.14.00",
     NAME: "Crawl Master II",
     YEAR: "2023",
     CSS: "color: #239AFF;",
